1a34f09a6a40c9833e18439184134003096d1452
Semciuc Nichita <nikitasemciuc@gmail.com>
2024-09-02 16:13:56 +0300

    -deprecated comms and hardcoded data

--- src/components/entities/headerControls/HeaderControls.tsx
+++ src/components/entities/headerControls/HeaderControls.tsx
@@ -1,37 +1,11 @@
-'use client';
-// import { useAppSelector } from '@/lib/utils/hooks';
 import css from './headerControls.module.scss';
-
-// import MainBtn from '@/components/shared/buttons/MainBtn';
-// import { useLogoutMutation } from '@/services/userApi';
-
-// import MainLink from '@/components/shared/links/MainLink';
-// import Cart from '@/components/shared/icons/Cart';
-// import FavoriteIcon from '@/components/shared/icons/FavoriteIcon';
-// import Badge from '../badge/Badge';
 
 import { Heart, ShoppingCart } from 'lucide-react';
 
 import { ChangeTheme, ProfileControls } from '@/components/features';
 import { MainLink } from '@/components/shared';
 
-const user = {
-  isActivated: false,
-  role: 'ADMIN',
-};
-
 export const HeaderControls = () => {
-  // const { user } = useAppSelector((state) => state.user);
-  // const inFavorite = useAppSelector((state) => state.favorite.items.length);
-  // const inCart = useAppSelector((state) => state.cart.items.length);
-
-  // const [logout] = useLogoutMutation();
-
-  // const logoutHandler = useCallback(() => {
-  //   logout(null);
-  //   localStorage.removeItem('isLogout');
-  // }, []);
-
   return (
     <>
       <div className={css.navButtons}>
