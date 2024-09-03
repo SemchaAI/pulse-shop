dd1b69bf525d80fb6ba953b0bcafdea97dfdade6
Semciuc Nichita <nikitasemciuc@gmail.com>
2024-09-03 16:34:57 +0300

    +fix
    default product list for each category

--- src/components/widgets/productsList/ProductsList.tsx
+++ src/components/widgets/productsList/ProductsList.tsx
@@ -1,49 +1,19 @@
 'use client';
-import { useEffect, useRef } from 'react';
-import { useIntersection } from 'react-use';
-
-import { useCategoryStore } from '@/stores';
-
-import type { ICategory } from '@/app/page';
-
 import css from './productsList.module.scss';
 import { ProductCard } from '@/components/entities';
+import { ICategory } from '@/models/searchWithParams';
 
 interface IProps {
   category: ICategory;
 }
 
 export const ProductsList = ({ category }: IProps) => {
-  const setActiveCategoryId = useCategoryStore((state) => state.setActiveId);
-
-  const intersectionRef = useRef(null);
-  const intersection = useIntersection(intersectionRef, {
-    threshold: 0.4,
-  });
-
-  //max two types of one product
-  const variants = 2;
-  //max of individual products
-  // variants * size = max items rendered in category
-  const size = 6;
-
-  useEffect(() => {
-    if (intersection?.isIntersecting) {
-      setActiveCategoryId(category.id);
-    }
-  }, [category.id, intersection?.isIntersecting, category.name]);
-
   return (
-    <div
-      ref={intersectionRef}
-      id={category.name}
-      className={css.container}
-    >
+    <div className={css.container}>
       <h3 className={css.title}>{category.name}</h3>
       <ul className={css.list}>
-        {category.products.slice(0, size).map((product) =>
-          product.productItem.slice(0, variants).map((item) => {
-            // console.log('item', item);
+        {category.products.map((product) =>
+          product.productItem.map((item) => {
             return (
               <ProductCard
                 key={item.id}
