e7d1dc4ab8ee33149a6939f3dbe024d7e28b0e85
Semciuc Nichita <nikitasemciuc@gmail.com>
2024-09-03 16:36:43 +0300

    +home page product list

---
+++ src/components/widgets/productsList/HomeProductsList.tsx
@@ -0,0 +1,67 @@
+'use client';
+import { useEffect, useRef } from 'react';
+import { useIntersection } from 'react-use';
+
+import { useCategoryStore } from '@/stores';
+
+import css from './productsList.module.scss';
+import { ProductCard } from '@/components/entities';
+import { ICategory } from '@/models/searchWithParams';
+import { MainLink } from '@/components/shared';
+
+interface IProps {
+  category: ICategory;
+}
+
+export const HomeProductsList = ({ category }: IProps) => {
+  const setActiveCategoryId = useCategoryStore((state) => state.setActiveId);
+
+  const intersectionRef = useRef(null);
+  const intersection = useIntersection(intersectionRef, {
+    threshold: 0.4,
+  });
+
+  //max two types of one product
+  const variants = 2;
+  //max of individual products
+  // variants * size = max items rendered in category
+  const size = 6;
+
+  useEffect(() => {
+    if (intersection?.isIntersecting) {
+      setActiveCategoryId(category.id);
+    }
+  }, [category.id, intersection?.isIntersecting, category.name]);
+
+  return (
+    <div
+      ref={intersectionRef}
+      id={category.name}
+      className={css.container}
+    >
+      <h3 className={css.title}>{category.name}</h3>
+      <ul className={css.list}>
+        {category.products.slice(0, size).map((product) =>
+          product.productItem.slice(0, variants).map((item) => {
+            // console.log('item', item);
+            return (
+              <ProductCard
+                key={item.id}
+                item={item}
+                product={product}
+              />
+            );
+          })
+        )}
+      </ul>
+      <MainLink
+        className={css.seeAll}
+        mode="button"
+        version="contain"
+        to={`/${category.name.toLowerCase()}`}
+      >
+        More {category.name}
+      </MainLink>
+    </div>
+  );
+};
