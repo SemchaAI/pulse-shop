3807735f07a8112829f20c081bed73b44558dd86
Semciuc Nichita <nikitasemciuc@gmail.com>
2024-09-03 16:31:47 +0300

    +fix
    toast to left
    favorite init from another place

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,20 +1,18 @@
 'use client';
 import { AnonymousSessionProvider } from '@/components/entities';
 import { toastOptions } from '@/utils/consts/ToastConfig';
-import { useFavorite } from '@/utils/hooks';
 import { SessionProvider } from 'next-auth/react';
 import { ThemeProvider } from 'next-themes';
 import { Toaster } from 'react-hot-toast';
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  useFavorite();
   return (
     <SessionProvider>
       <AnonymousSessionProvider>
         <ThemeProvider defaultTheme="light">
           {children}
           <Toaster
-            position="top-right"
+            position="top-left"
             toastOptions={toastOptions}
           />
         </ThemeProvider>
