5d8a6b46afa2c120062cf64030f5f6f02c009474
Semciuc Nichita <nikitasemciuc@gmail.com>
2024-09-04 11:19:15 +0300

    +cSpell warning fix

--- src/utils/helpers/getUserSession.ts
+++ src/utils/helpers/getUserSession.ts
@@ -2,7 +2,7 @@ import { getServerSession } from 'next-auth';
 import { authOptions } from '../consts/authOptions';
 import { IUserSession } from '@/models/user';
 
-//dont use reexport, its only server func, can be problem in client
+//don't use reexport, its only server func, can be problem in client
 export const getUserSession = async (): Promise<IUserSession | null> => {
   const session = await getServerSession(authOptions);
 
