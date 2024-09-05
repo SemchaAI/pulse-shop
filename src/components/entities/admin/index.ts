dfc19e9773d92f4c5530c396995c8cf3f3cd0944
Semciuc Nichita <nikitasemciuc@gmail.com>
2024-09-05 14:50:07 +0300

    +reexport

--- src/components/entities/admin/index.ts
+++ src/components/entities/admin/index.ts
@@ -1,11 +1,17 @@
 export * from './forms/simpleForm/AddCategory';
 export * from './forms/simpleForm/AddColor';
+export * from './forms/simpleForm/AddMemory';
+export * from './forms/simpleForm/AddRam';
 
 export * from './forms/simpleForm/DeleteCategory';
 export * from './forms/simpleForm/DeleteColor';
+export * from './forms/simpleForm/DeleteMemory';
+export * from './forms/simpleForm/DeleteRam';
 
 export * from './infoList/InfoList';
 
 export * from './infoCard/InfoCard';
 
 export * from './forms/simpleForm/SimpleForm';
+
+export * from './forms/simpleForm/SimpleForm2';
