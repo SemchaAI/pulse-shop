7bde6fc1c546da1350e8cc064e32f62dd0649809
Semciuc Nichita <nikitasemciuc@gmail.com>
2024-09-10 15:23:52 +0300

    +products models for admin api

--- src/models/product.ts
+++ src/models/product.ts
@@ -19,21 +19,36 @@ export interface IProductInfo {
   };
   productItem: ProductItem;
 }
+export interface IInfoProduct {
+  info: {
+    title: string;
+    description: string;
+  }[];
+  productId: string;
+}
 
-// where: {
-//   id: Number(productId),
-// },
-// include: {
-//   productItem: {
-//     where: {
-//       id: Number(productItemId),
-//     },
-//     include: {
-//       productImages: true,
-//     },
-//   },
-//   colors: true,
-//   memory: true,
-//   category: true,
-//   productInfo: true,
-// },
+export interface IProductItem {
+  title: string;
+  cnt: number;
+  img: string;
+
+  oldPrice: number | null;
+  price: number;
+
+  productId: number;
+  colorId: string | null;
+  memoryId: string | null;
+  ramId: string | null;
+}
+
+export interface IDeleteProductItem {
+  productId: string;
+  colorId: string | null;
+  memoryId: string | null;
+  ramId: string | null;
+}
+
+export interface IProductThumbnails {
+  thumbnails: string;
+  productItemId: string;
+}
