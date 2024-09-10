5ebc0492cf91ecb8b082da7ea9a926154c3a2650
Semciuc Nichita <nikitasemciuc@gmail.com>
2024-09-10 15:40:22 +0300

    +reexport admin

--- src/components/entities/admin/index.ts
+++ src/components/entities/admin/index.ts
@@ -2,13 +2,25 @@ export * from './forms/simpleForm/AddCategory';
 export * from './forms/simpleForm/AddColor';
 export * from './forms/simpleForm/AddMemory';
 export * from './forms/simpleForm/AddRam';
+export * from './forms/simpleForm/AddSlide';
+export * from './forms/productForm/AddProduct';
+
+export * from './forms/productForm/AddProductItem';
+export * from './forms/productForm/ProductInfoForm';
+export * from './forms/productForm/AddThumbnails';
 
 export * from './forms/simpleForm/DeleteCategory';
 export * from './forms/simpleForm/DeleteColor';
 export * from './forms/simpleForm/DeleteMemory';
 export * from './forms/simpleForm/DeleteRam';
 
+export * from './forms/productForm/DeleteProduct';
+export * from './forms/productForm/DeleteProductInfo';
+export * from './forms/productForm/DeleteProductItem';
+export * from './forms/productForm/DeleteThumbnails';
+
 export * from './infoList/InfoList';
+export * from './slideList/SlideList';
 
 export * from './infoCard/InfoCard';
 
