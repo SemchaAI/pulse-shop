b09a618116c85a1b0f4653991b4a5bcc5de59b7f
Semciuc Nichita <nikitasemciuc@gmail.com>
2024-09-10 15:14:54 +0300

    +slides from backend for banner

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -2,10 +2,11 @@ import { Container } from '@/components/shared';
 import { Filters, ProductsSection, TopBar } from '@/components/widgets';
 
 import { findCategoriesWithParams } from '@/utils/helpers/findCategoriesWithParams';
-import css from './assets/home/page.module.scss';
+import css from '@/app/assets/home/page.module.scss';
 import { HeaderBanner } from '@/components/entities';
-import { OPTIONS, SLIDES } from '@/utils/consts/HeaderBanner';
+import { OPTIONS } from '@/utils/consts/HeaderBanner';
 import { IParams } from '@/models/searchWithParams';
+import { prisma } from '@/prisma/prisma-client';
 
 export default async function Home({
   searchParams,
@@ -14,11 +15,12 @@ export default async function Home({
 }) {
   //@categories
   const categories = await findCategoriesWithParams(searchParams);
+  const slides = await prisma.bannerSlide.findMany();
   return (
     <>
       <TopBar categories={categories} />
       <HeaderBanner
-        slides={SLIDES}
+        slides={slides}
         options={OPTIONS}
       />
       <Container>
