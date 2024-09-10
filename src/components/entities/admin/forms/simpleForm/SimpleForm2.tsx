b05e9e458c5557ec4085729f2a5310c1faa3b8e5
Semciuc Nichita <nikitasemciuc@gmail.com>
2024-09-10 15:43:22 +0300

    +update
    +selects option adding for form
    +default select option with no value

--- src/components/entities/admin/forms/simpleForm/SimpleForm2.tsx
+++ src/components/entities/admin/forms/simpleForm/SimpleForm2.tsx
@@ -5,6 +5,7 @@ import {
   DefaultValues,
   FieldValues,
   FormProvider,
+  Path,
   useForm,
 } from 'react-hook-form';
 import toast from 'react-hot-toast';
@@ -17,16 +18,22 @@ interface IField extends IInputTypes {
   id: string;
   label: string;
   validation: ValidationTypes;
+  myType: IInputTypes['myType'];
 }
 interface IProps<T> {
   defaultValues: DefaultValues<T>;
-  data: IField[];
+  data?: IField[];
   title: string;
   request(data: T): Promise<void>;
+  selects?: ISelect[];
+}
+interface ISelect {
+  name: string;
+  options: { name: string; id: number }[];
 }
 
 export const SimpleForm2 = <T extends FieldValues>(props: IProps<T>) => {
-  const { defaultValues, data, title, request } = props;
+  const { defaultValues, data, title, request, selects } = props;
   const form = useForm({
     defaultValues,
     mode: 'onBlur',
@@ -50,7 +57,29 @@ export const SimpleForm2 = <T extends FieldValues>(props: IProps<T>) => {
           onSubmit={form.handleSubmit(submitHandler)}
         >
           <h3 className={css.title}>{title}</h3>
-          {data.map((field) => (
+          {selects?.map((select) => (
+            <select
+              key={select.name}
+              id={select.name}
+              {...form.register(select.name as Path<T>)}
+            >
+              <option
+                key={-1}
+                value=""
+              >
+                No data
+              </option>
+              {select.options.map((option) => (
+                <option
+                  key={option.id}
+                  value={option.id}
+                >
+                  {option.name}
+                </option>
+              ))}
+            </select>
+          ))}
+          {data?.map((field) => (
             <Field
               key={field.id}
               id={field.id}
