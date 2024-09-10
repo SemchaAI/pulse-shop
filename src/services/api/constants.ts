2369c8cfa6e81d2fb2c80e13a14128c6d3611db8
Semciuc Nichita <nikitasemciuc@gmail.com>
2024-09-10 15:23:31 +0300

    +api admin routes

--- src/services/api/constants.ts
+++ src/services/api/constants.ts
@@ -8,6 +8,11 @@ export enum ApiRoutes {
   ADMIN_COLOR = 'admin/color',
   ADMIN_MEMORY = 'admin/memory',
   ADMIN_RAM = 'admin/ram',
+  ADMIN_BANNER = 'admin/banner',
+  ADMIN_PRODUCT = 'admin/product',
+  ADMIN_PRODUCT_INFO = 'admin/product/info',
+  ADMIN_PRODUCT_ITEM = 'admin/product/item',
+  ADMIN_PRODUCT_IMAGES = 'admin/product/images',
 
   CART = 'cart',
   FILTER_COLORS = 'filters/colors',
