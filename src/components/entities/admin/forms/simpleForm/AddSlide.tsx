ee74348e08fe47ad0e9e81d6197946489b1c8f1a
Semciuc Nichita <nikitasemciuc@gmail.com>
2024-09-10 15:32:27 +0300

    +homepage banner admin controls

---
+++ src/components/entities/admin/forms/simpleForm/AddSlide.tsx
@@ -0,0 +1,68 @@
+'use client';
+import { SimpleForm2 } from '@/components/entities';
+import { api } from '@/services/api/baseApi';
+import { required } from '@/utils/consts/validationObjects';
+import { BannerSlide } from '@prisma/client';
+
+export const AddSlide = () => {
+  const defaultValues: Omit<BannerSlide, 'id'> = {
+    desktop: '',
+    tablet: '',
+    mobile: '',
+    alt: '',
+    href: '',
+    base64:
+      'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7',
+    priority: true,
+    width: 1340,
+    height: 560,
+  };
+
+  return (
+    <SimpleForm2
+      defaultValues={defaultValues}
+      data={[
+        {
+          id: 'desktop',
+          label: 'Desktop url',
+          validation: required,
+          myType: 'text',
+        },
+        {
+          id: 'tablet',
+          label: 'Tablet url',
+          validation: required,
+          myType: 'text',
+        },
+        {
+          id: 'mobile',
+          label: 'Mobile url',
+          validation: required,
+          myType: 'text',
+        },
+        {
+          id: 'alt',
+          label: 'Description',
+          validation: required,
+          myType: 'text',
+        },
+        {
+          id: 'href',
+          label: 'url to product',
+          validation: required,
+          myType: 'text',
+        },
+        {
+          id: 'base64',
+          label: 'Base64',
+          validation: required,
+          myType: 'text',
+        },
+      ]}
+      title="Add Banner"
+      request={async (data) => {
+        await api.banner.createOne(data);
+      }}
+    />
+  );
+};
---
+++ src/services/bannerApi.ts
@@ -0,0 +1,20 @@
+import server from './api/server';
+import { ApiRoutes } from './api/constants';
+import { BannerSlide } from '@prisma/client';
+
+type TBannerSlide = Omit<BannerSlide, 'id'>;
+
+export const createOne = async (slide: TBannerSlide): Promise<TBannerSlide> => {
+  console.log('slide', slide);
+  const { data } = await server.post<TBannerSlide>(
+    ApiRoutes.ADMIN_BANNER,
+    slide
+  );
+  return data;
+};
+export const deleteOne = async (name: { id: number }): Promise<BannerSlide> => {
+  const { data } = await server.delete<BannerSlide>(ApiRoutes.ADMIN_BANNER, {
+    data: name,
+  });
+  return data;
+};
