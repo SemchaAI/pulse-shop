34a3ee96cea53fd9b7c238736edbab9ff7ff9e6a
Semciuc Nichita <nikitasemciuc@gmail.com>
2024-09-10 15:38:07 +0300

    +delete slide admin component

---
+++ src/components/entities/admin/slideList/SlideList.tsx
@@ -0,0 +1,63 @@
+'use client';
+import Image from 'next/image';
+import css from './slideList.module.scss';
+import { BannerSlide } from '@prisma/client';
+import { MainBtn } from '@/components/shared';
+import { Trash } from 'lucide-react';
+import { deleteUTFiles } from '@/app/actions';
+import toast from 'react-hot-toast';
+import { api } from '@/services/api/baseApi';
+
+interface IProps {
+  slides: BannerSlide[];
+}
+
+export const SlideList = ({ slides }: IProps) => {
+  const clickHandler = async (files: string[], id: number) => {
+    try {
+      await deleteUTFiles(files);
+      await api.banner.deleteOne({ id });
+      toast.success('Slide deleted');
+    } catch (error) {
+      toast.error('Slide deleted failed');
+      console.log(error);
+    }
+  };
+  return (
+    <div className={css.slideList}>
+      {slides.map((slide) => (
+        <div
+          className={css.slide}
+          key={slide.id}
+        >
+          <h3>{slide.alt}</h3>
+          <div className={css.slideImg}>
+            <Image
+              style={{ objectFit: 'contain', maxWidth: '300px' }}
+              width={slide.width}
+              height={slide.height}
+              src={slide.desktop}
+              alt={slide.alt}
+            />
+            <MainBtn
+              version="outline"
+              icon
+              onClick={() =>
+                clickHandler(
+                  [
+                    slide.desktop.split('/f/')[1],
+                    slide.tablet.split('/f/')[1],
+                    slide.mobile.split('/f/')[1],
+                  ],
+                  slide.id
+                )
+              }
+            >
+              <Trash />
+            </MainBtn>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
