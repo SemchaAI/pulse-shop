0d4953d8dfb7737f211fce80574c8be7bae12bce
Semciuc Nichita <nikitasemciuc@gmail.com>
2024-11-29 11:14:21 +0200

    +dynamic render

--- src/app/(admin)/admin/banner/page.tsx
+++ src/app/(admin)/admin/banner/page.tsx
@@ -3,6 +3,9 @@ import { UploadThing } from '@/components/features';
 import { Container } from '@/components/shared';
 import { prisma } from '@/prisma/prisma-client';
 import { OPTIONS } from '@/utils/consts/HeaderBanner';
+
+export const dynamic = 'force-dynamic';
+
 export default async function BannerPage() {
   const slides = await prisma.bannerSlide.findMany();
   return (
